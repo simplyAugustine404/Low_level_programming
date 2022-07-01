7bd249ce4a673c47819cadb806efd2fe29a7a1b9
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-07-01 13:06:31 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}
