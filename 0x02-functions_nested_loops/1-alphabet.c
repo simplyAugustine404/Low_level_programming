342ca05ebcccbd7ecf3063358aa79591e60b2d0c
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-07-05 03:05:43 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,16 @@
  *
  * Return: Always 0.
  */
-/*int main(void)
-{
-	print_alphabet();
-	return (0);
+/**int main(void)
+*{
+*	print_alphabet();
+*	return (0);
 }*/
 void print_alphabet(void)
 {
 	int i;
 
-	for(i = 97; i <= 122; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
