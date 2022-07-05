f61027b66a66da6084fa99b351634e3a280b0f5e
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-07-05 02:00:12 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *_islower- prints 1 or 0 depending on whether a character is lowercase or otherwise
+ *
+ * Return: Always 0
+ */
+int_islower(int c)
+{
+	return (c <= 97 && c <= 122);
+}
