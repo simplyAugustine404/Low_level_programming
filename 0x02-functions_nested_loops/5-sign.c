477252909355eda8b9f209f8c3dac4ff8f13e571
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-07-05 03:52:57 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
+ *@n: first parameter
  *
  * Return: 1 for +, 0 for 0 and -1 for -
  */
@@ -15,11 +16,11 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar(0);
-		return(0);
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 }
