ae5858a89a58ef421cf3d8cdc3e8ef4c2b2e5b49
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-07-07 03:23:02 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,10 +7,11 @@
 
 void print_triangle(int size)
 {
-	if (size <= 0)
-		_putchar('\n');
 	int i, j;
 
+	if (size <= 0)
+		_putchar('\n');
+	
 	for (i = 1; i <= size; i++)
 	{
 		for (j = i; j <= size; j++)
