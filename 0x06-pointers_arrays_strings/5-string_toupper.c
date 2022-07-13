ec4afc33814ddb8122754c0e686df3c6c40c991d
Shawn Kiplagat <kiplagatshawn@gmail.com>
2022-07-13 05:15:56 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * string_toupper - converts lowercase to uppercase
+ * @s: input string
+ * Return: pointer to destination
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) = *(s + i) - 32;
+		i++;
+	}
+
+	return (s);
+}
